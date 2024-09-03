28260b1df6b9abb0c92c90ece322da02452e8b72
Sharul Shah <sharulshah@icloud.com>
2024-09-03 10:36:01 -0500

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -1,33 +1,6 @@
 import pygame
 import random
 import math
-import turtle 
-
-turtle.forward(10)
-
-pygame.init()
-
-display = pygame.display.set_mode([600, 600])
-clock = pygame.time.Clock()
-FPS = 60
 
 white = (255, 255, 255)
 black = (0, 0, 0)
-
-def game():
-    
-    while True:    
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit() 
-            
-            display.fill(white)   
-                        
-            pygame.display.update()
-
-            clock.tick(FPS)
-        
-game()
-
-
-    
