3a5a2c95fdbf27ce7780a6702d49439b5a860e5a
Sharul Shah <sharulshah@icloud.com>
2024-09-14 10:46:24 -0500

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -1,6 +1,17 @@
-import pygame
 import random
 import math
 
 white = (255, 255, 255)
 black = (0, 0, 0)
+
+inputArray = input("Input numbers + operators : ").split()
+for i in inputArray:
+    try:
+        # fix this stuff
+        if isinstance(int(i), int):
+            print("a")
+        else: 
+            print("b")
+            
+    except TypeError:
+        break
