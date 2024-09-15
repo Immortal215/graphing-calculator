a5ea7db705e118b82c26e56da7d5bf532fdf8b45
Sharul Shah <sharulshah@icloud.com>
2024-09-15 14:04:42 -0500

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -1,17 +1,21 @@
 import random
 import math
+import os
+from functionsFile import *
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-numArray = []
-operatorArray = []
-
 inputArray = input("Input numbers + operators : ").split()
+
 for i in inputArray:
     try:
-        # fix this stuff
-       numArray.append(int(i))
-        
+        if intCheck(i):
+            print("a")
+        else: 
+            print("b")
+            
     except TypeError:
-        operatorArray.append(i)
+        break
+    
+    
