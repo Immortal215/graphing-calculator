79aaa1fea3428af1cfb623d4ed79f24a2e50d992
Sharul Shah <sharulshah@icloud.com>
2024-09-15 14:46:28 -0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -6,16 +6,29 @@ from functionsFile import *
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-inputArray = input("Input numbers + operators : ").split()
+# base arrays
+operatorArray = ["+", "-", "*", "/"]
+operatorList = []
+numList = []
 
-for i in inputArray:
-    try:
+def calculate(inputArray):
+    operatorList = []
+    numList = []
+    
+    for i in inputArray:
         if intCheck(i):
-            print("a")
+            print("int")
+            numList.append(int(i))
         else: 
-            print("b")
-            
-    except TypeError:
-        break
-    
-    
+            if i in operatorArray:
+                print("\noperator\n")
+                operatorList.append(i)
+            else:
+                print("Not an operator\n")
+                calculate(input("Input numbers + operators : ").split())
+        for i in range(0, len(inputArray), 2):
+            if not intCheck(inputArray[i]):
+                print("\nNot correct input notation\n")
+                calculate(input("Input numbers + operators : ").split())
+
+calculate(input("Input numbers + operators : ").split())
